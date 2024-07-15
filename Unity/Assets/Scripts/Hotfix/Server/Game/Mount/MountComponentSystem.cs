bcc8e80bc0f9446832da81600557bbd3ca1a6901
zeusyu <707073900@qq.com>
2024-07-15 12:07:50 +0800

    【fix】基础功能

---
+++ Unity/Assets/Scripts/Hotfix/Server/Game/Mount/MountComponentSystem.cs
@@ -0,0 +1,38 @@
+﻿namespace ET.Server
+{
+    [EntitySystemOf(typeof(MountComponent))]
+    [FriendOfAttribute(typeof(ET.Server.MountComponent))]
+    public static partial class MountComponentSystem
+    {
+        [EntitySystem]
+        private static void Awake(this ET.Server.MountComponent self)
+        {
+        }
+
+        [EntitySystem]
+        private static void Destroy(this ET.Server.MountComponent self)
+        {
+        }
+
+        [EntitySystem]
+        private static void Deserialize(this ET.Server.MountComponent self)
+        {
+        }
+
+        public static void ActivationMount(this MountComponent self, int mountConfigId)
+        {
+            if (self.Mounts.Contains(mountConfigId))
+            {
+                return;
+            }
+
+            MountConfig mountConfig = MountConfigCategory.Instance.Get(mountConfigId);
+            if (mountConfig == null)
+            {
+                return;
+            }
+
+            self.Mounts.Add(mountConfigId);
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/Model/Server/Game/Mount/MountComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ET.Server
+{
+    [ComponentOf]
+    [UnitCacheEvent(typeof(MountComponent))]
+    public class MountComponent : Entity, IAwake, IDestroy, ITransfer, IDeserialize
+    {
+        public int MountConfig;
+        
+        public List<int> Mounts = new();
+    }
+}
