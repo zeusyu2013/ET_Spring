bb1367d541f60c0cb6c9be8f691918b694db853c
zeusyu <707073900@qq.com>
2024-09-03 20:29:59 +0800

    【fix】基础功能

--- Unity/Assets/Scripts/Hotfix/Server/Game/Soul/SoulTalent/SoulTalentComponentSystem.cs
+++ Unity/Assets/Scripts/Hotfix/Server/Game/Soul/SoulTalent/SoulTalentComponentSystem.cs
@@ -1,6 +1,7 @@
 ﻿namespace ET.Server
 {
     [EntitySystemOf(typeof(SoulTalentComponent))]
+    [FriendOfAttribute(typeof(ET.Server.Soul))]
     public static partial class SoulTalentComponentSystem
     {
         [EntitySystem]
@@ -11,6 +12,17 @@
         [EntitySystem]
         private static void Destroy(this ET.Server.SoulTalentComponent self)
         {
+        }
+
+        public static void Add(this SoulTalentComponent self, int configId)
+        {
+            Soul soul = self.GetParent<Soul>();
+
+            SoulTalentConfig config = SoulTalentConfigCategory.Instance.Get(soul.ConfigId);
+            if (config == null)
+            {
+                return;
+            }
         }
     }
 }
--- Unity/Assets/Scripts/Model/Server/Game/Soul/SoulTalent/SoulTalentComponent.cs
+++ Unity/Assets/Scripts/Model/Server/Game/Soul/SoulTalent/SoulTalentComponent.cs
@@ -1,7 +1,13 @@
-﻿namespace ET.Server
+﻿using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Options;
+
+namespace ET.Server
 {
     [ComponentOf(typeof(Soul))]
     public class SoulTalentComponent : Entity, IAwake, IDestroy, ISerializeToEntity
     {
+        [BsonDictionaryOptions(DictionaryRepresentation.ArrayOfArrays)]
+        public Dictionary<int, int> SoulTalents = new();
     }
 }
